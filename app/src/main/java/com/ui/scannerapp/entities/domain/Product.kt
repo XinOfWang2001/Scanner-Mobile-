b3c1781d64491da63dfe1957586b93c581236fda
XinOfWang2001 <Xin20Wang@outlook.com>
2026-01-30 11:25:48 +0100

    Domain class Cart tested and basics implemented

--- app/src/main/java/com/ui/scannerapp/entities/domain/Product.kt
+++ app/src/main/java/com/ui/scannerapp/entities/domain/Product.kt
@@ -1,4 +1,18 @@
 package com.ui.scannerapp.entities.domain
 
-class Product(val Id: Int, val LabelId: String, val Name: String, val Price: Float) {
+class Product {
+    val id: Int
+    val labelId: String
+    val name: String
+    val price: Float
+    val type: String
+
+
+    constructor(id: Int, labelId: String, name: String, type: String, price: Float) {
+        this.id = id
+        this.labelId = labelId
+        this.name = name
+        this.type = type
+        this.price = price
+    }
 }
