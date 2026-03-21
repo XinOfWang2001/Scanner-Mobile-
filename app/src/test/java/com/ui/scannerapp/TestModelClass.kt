c619f5ad69c52e8aa65a85426e76f70cd86c347b
XinOfWang2001 <Xin20Wang@outlook.com>
2026-03-21 23:58:32 +0100

    cREATE STUB Class for testing model inference

---
+++ app/src/test/java/com/ui/scannerapp/TestModelClass.kt
@@ -0,0 +1,13 @@
+package com.ui.scannerapp
+
+import org.junit.Assert.assertEquals
+import org.junit.Test
+
+class TestModelClass{
+
+    @Test
+    fun addition_isCorrect_2() {
+        assertEquals(4, 2 + 2)
+    }
+}
+
