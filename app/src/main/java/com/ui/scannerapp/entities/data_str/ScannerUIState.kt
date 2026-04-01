f2a0165784177e9d282094991cb4262dc4d6e054
XinOfWang2001 <Xin20Wang@outlook.com>
2026-04-01 15:11:30 +0200

    Automatic inference when capturing images

--- app/src/main/java/com/ui/scannerapp/entities/data_str/ScannerUIState.kt
+++ app/src/main/java/com/ui/scannerapp/entities/data_str/ScannerUIState.kt
@@ -5,7 +5,8 @@ import android.net.Uri
 data class ScannerUiState(
     val capturedImage: Uri? = null,
     val isProcessing: Boolean = false,
-    val processingResult: String? = null,
+    val infoMessages: String? = null,
     val errorMessage: String? = null,
+    val predictions: List<DetectedBox> = emptyList(),
     val detectedBoxes: List<DetectedBox> = emptyList()
 )
